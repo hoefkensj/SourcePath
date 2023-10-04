4c46c6b717ac953146b8778fa3d8398e133a328d
hoefkensj <hoefkens.j@gmail.com>
2023-10-04 19:11:47 +0200

    install.sh, SourcePath-0.60.sh, and SourcePath-0.99.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 # ############################################################################
-# # PATH: bash_scripts/Bash_SourceDir_installer  AUTHOR: Hoefkens.J@gmail.com
+# # PATH: bash_scripts/sourcepath_installer  AUTHOR: Hoefkens.J@gmail.com
 # # FILE: install.sh                                   0v7 - 2023.04.26
 # ############################################################################
 #
 
-latest=$(ls "$PWD/src/bash_sourcedir-"*|sort -n |tail -n 1 )
-ln -rsvf "$latest" "$PWD/bash_sourcedir.sh"
-ln -svf "$PWD/src/bash_sourcedir.sh" "/etc/profile.d/sourcedir.sh"
-source "/etc/profile.d/bash_sourcedir.sh"
-bash_sourcedir
+INSTALLDIR=/opt/local/scripts/bash/SourcePath/
+mkdir -p $INSTALLDIR
+latest=$(ls "$PWD/src/SourcePath-"*|sort -n |tail -n 1 )
+cp $latest $INSTALLDIR
+cd $INSTALLDIR
+installed=$(ls "$PWD/SourcePath-"*|sort -n |tail -n 1 )
+ln -rsvf "$installed" "$PWD/SourcePath.sh"
+ln -svf "$PWD/SourcePath.sh" "/etc/profile.d/SourcePath.sh"
+source "/etc/profile.d/SourcePath.sh"
+sourcepath
 echo "Symlink installed in : /etc/profile.d/" 
 unset latest
